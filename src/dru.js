49b8e57ca143fcdd95565f846fa1c86b3637d1c6
Dru Sellers <dru@drusellers.com>
2018-09-08 09:05:14 -0500

    updated build with CDN busting

--- src/dru.js
+++ src/dru.js
@@ -1,5 +1,6 @@
 //import hljs from './js/highlight.min.js';
 import './css/highlight.css';
+import 'tachyons/css/tachyons.css';
 
 console.log("yo");
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,37 @@
 var path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/dru.js',
+  plugins: [
+    // keep the output dir clean
+    new CleanWebpackPlugin(['static/js']),
+    // improve the consistency of hashes on the output name
+    new webpack.HashedModuleIdsPlugin(),
+    // connect the hashed ids to hugo
+    new ManifestPlugin({
+      // used to read in the file path in hugo template
+      fileName: '../data/manifest.json'
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, 'static'),
-    filename: 'js/dru.bundle.js'
+    filename: 'js/[name].[contenthash].js'
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
   },
   module: {
     rules: [{
