85a04ccfdfd4e1d70a6ffec6493ccc96089e434a
Dru Sellers <dru@drusellers.com>
2018-09-14 10:46:07 -0500

    Webpack harder

--- src/dru.js
+++ src/dru.js
@@ -1,6 +1,9 @@
 //import hljs from './js/highlight.min.js';
 import './css/highlight.css';
+
 import 'tachyons/css/tachyons.css';
+import 'tachyons-cms/css/tachyons-cms.css';
+
 import retina from 'retinajs';
 
 window.addEventListener('load', retina);
