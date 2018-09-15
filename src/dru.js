f40be6486cb8715c571a1b6bfa93c6bcf267dd2f
Dru Sellers <dru@drusellers.com>
2018-09-15 09:55:24 -0500

    improve cache control

--- src/dru.js
+++ src/dru.js
@@ -2,11 +2,22 @@
 import './css/highlight.css';
 import './css/nested.css';
 
+// import '@fortawesome/fontawesome-pro/css/light.css';
+// import '@fortawesome/fontawesome-pro/css/brands.css';
+// import fontawesome from '@fortawesome/fontawesome-pro';
+// import faTag from '@fortawesome/fontawesome-pro-light/faTag';
+// fontawesome.library.add(faTag);
+// fontawesome.dom.i2svg();
+
+// fa fa-external-link-square | fa-external-link
+// fal fa-camera
+// fab fa-creative-commons
+// fas fa-search
+
 import 'tachyons/css/tachyons.css';
 import 'tachyons-cms/css/tachyons-cms.css';
 
 import retina from 'retinajs';
-
 window.addEventListener('load', retina);
 
 console.log("yo");
