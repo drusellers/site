b56677adfc6bf7bd9741384b22549d8ae23d92c8
Dru Sellers <dru@drusellers.com>
2020-03-04 06:11:25 -0600

    more ELO work

--- src/search/SearchBox.jsx
+++ src/search/SearchBox.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 function SearchBox({ query, onQueryChange }) {
   return (
-    <div className="fl w-100 dds-search-input br2" style={{ display: 'flex' }}>
-      <div className="fl" style={{ width: '35px', paddingTop: '11px', paddingLeft: '1rem' }}>
+    <div className="dds-search-input">
+      <div className="dds-search-icon">
         <i className="far fa-search"></i>
       </div>
-      <div className="fl pa2" style={{ flex: 1 }}>
+      <div className="dds-search-input-wrapper">
         <input type="text"
-          className="input-reset f4 ba b--white w-100 db"
           aria-label="Search Posts"
           placeholder="Search Posts"
           onChange={e => {
