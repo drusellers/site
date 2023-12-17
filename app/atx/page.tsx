a6718811b8f477d5aa5090c0b5ea9ea741fa1e37
Dru Sellers <dru@drusellers.com>
2023-12-17 11:45:40 -0600

    atx initial

---
+++ app/atx/page.tsx
@@ -0,0 +1,76 @@
+export default function AtxPage() {
+  return (
+    <div className={'prose'}>
+      1. Austin is very proud of its BBQ. So if you like Texas BBQ (think
+      Brisket) then that&apos;s a must do. Terry Blacks BBQ on Barton Springs La
+      BBQ on Caesar Chavez Franklins (but that&apos;s a long multi-hour line -
+      the two above are just as good) Oh, by my house is Moreno BBQ which is an
+      upcoming rock star 2. Night Life: The major nightlife is going to be 6th
+      street and Rainey St. 6th street is divided into 3 sections (West 6th,
+      Dirty, and East 6th). West is more clubby, Dirty is … well dirty - think
+      the kinds of places you could sneak in under age - dollar shots - etc.
+      East is the en vogue area and has more cocktails and hipster ish vibe.
+      East 6th has my preferred cocktail bar called Whislers - and has an
+      upstairs Mezcal bar. Across the street behind Lefty&apos;s is a dedicated
+      Gin bar. East 6th also has my favorite “two-stepping bar” called White
+      Horse that has Whiskey on tap as well. Rainey - is starting to fall out of
+      my favor as it becomes condo heavy. But people still remember it as a cool
+      place. There is a fun dive bar up on 11th called Nickel City that could be
+      a fun post-music stop. since its in-between the two destinations. 3.
+      Nature: Number one is my heart is Barton Springs. If y&apos;all brought
+      swimming&apos; suits and the weather is hot enough - I absolutely
+      recommend a trip here. It&apos;s a natural spring fed pool that is 68
+      degrees year round. You go and bask in the texas heat, then take a dip in
+      the chilly waters. It&apos;s a huge lay on the grass situation. (No food
+      or drinks allowed besides water to help keep pests out) After wards the
+      move is to and get trashy food. This is a good recovery day. Options
+      include Pool Burger, Matt&apos;s El Rancho, or if you are really feeling
+      your college days going to Baby A&apos;s and getting shit housed on
+      “Purps” (an ever clear based purple sugar “margarita”) also the Green Belt
+      hikes are a strong move if you want to get between 1-4 miles of walking in
+      surrounded by trees and if we have had enough water, some running water
+      too. Plenty of entrances and google can help you. Also, the Town Lake
+      trail - it&apos;s a 2-6 mile hike and bike trail around the “lake” in the
+      center of the city. I say “lake” because it&apos;s damned up river. 😉 and
+      it looks like a river. Tons of people will be out on the trail so it can
+      be a fun time. Vibing with the College kids? … Austin is the Home to the
+      great University of Texas which has a huge campus and a neat vibe. If you
+      want to walk around college kids or if you want to get some UT gear for a
+      fan back home, I recommend going to the “Co Op” (a three story store
+      dedicated to all things burnt orange) on Guadalupe. From there you can
+      easily walk around check the scene. Now since you are used to really good
+      beef, if you are looking for a good night out with high quality meat I
+      would recommend Jacoby&apos;s on Caesar Chavez. If you want Sushi - I
+      recommend Uchi, Fukumoto, or Neighborhood Sushi. High End Mexican food:
+      ATX Cocina Middle End Mexican Food: De Nada Cantina - get the pink cup
+      Margs but be warned…. If you want a solid Tex-Mex hit - Matt&apos;s El
+      Rancho - the Knockout Margaritas and “The Bob” queso. Austin is also a
+      huge fan of breakfast tacos. You can pretty much throw a rock and hit a
+      place serving them. Brunch: Little Darlin - near me - also good for
+      dinners. A former motorcyclist bar taken over by hipsters (Yellow Jacket
+      Social Club is the same kind of vibe) Paper Boy (upscale) - if you can get
+      it - that place is busy AF Fresas on South 1st - strong mexican food
+      location in a hip part of town. Murals: Austin has a few murals - but the
+      best one is to get a picture taken in front of the “I Love you So Much”
+      mural on South Congress. Also, South Congress is a great place to park and
+      walk up and down the street. Lot&apos;s of food options and people
+      watching is A+. …pausing… let me know if I missed anything you were
+      thinking of. Breweries: PHP (Pint House Pizza): Get the Electric Jellyfish
+      (a hazy IPA that&apos;s all the rage) Meanwhile Brewery: A bit further
+      East St. Elmo Brewery - in an up and coming little spot. Has a great food
+      truck called Spicy Boys - It&apos;s also right next to Still Austin for
+      local spirits. In the St. Elmo place is also a winery. Just down the
+      “block” Groceries - near my house: 1. Central Market - upscale HEB grocery
+      store with a limited selection but great produce and an interesting “maze
+      like” layout. Def not what you see every day. 2. HEB - here is another
+      thing that Texans will brag about until the cows come home. Their love of
+      HEB. This is your one stop shop grocery store (Where CM will be missing
+      odds and ends like not $1,000 toilet paper). This feels and looks like a
+      normal store, they just tend to be a bit bigger and because we are in
+      texas the produce is 💯 3. Sprouts also close - hipster grocery - can be
+      hit and miss on general items but if you need an organic GMO free, blessed
+      by the fairies at night shaker of salt. They got you fam. Don&apos;t Do: I
+      don&apos;t recommend the Randalls that is close to me.
+    </div>
+  )
+}
